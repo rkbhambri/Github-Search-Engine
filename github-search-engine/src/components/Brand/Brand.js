4dbd69a51effb2de90894b2fb7e9c3f138e04a8b
ram <ramkrishan@kontikilabs.com>
2019-03-13 18:18:30 +0530

    Minor fix in UI

--- github-search-engine/src/components/Brand/Brand.js
+++ github-search-engine/src/components/Brand/Brand.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Brand = (props) => {
     return (
-        <div className="brand col-md-3 border">
+        <div className="brand col-md-3">
             <h3>Github Search Engine</h3>
         </div>
     );
